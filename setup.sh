e7f1dc45b44ed725f91657d2cfaf70edfd82e4cb
Hung-Tien Huang <32878707+busFred@users.noreply.github.com>
2025-05-20 20:18:35 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
+pip install --no-cache-dir -r requirements.txt --use-pep517
 pip install -e ./datasets --use-pep517
 pip install -e ./models --use-pep517
+pip install -e ./libs/internal --use-pep517
